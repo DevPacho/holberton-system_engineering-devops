8e51f4306bb4ebf36558bb0dfb4e6760b6221bba
DevPacho <frankjose2004@gmail.com>
2022-06-24 20:37:06 -0500

    Header file with libraries and function prototypes

---
+++ 0x05-processes_and_signals/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* LIBRARIES */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+
+int infinite_while(void);
+int create_zombies(void);
+
+#endif
