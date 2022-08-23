3763ff01b0ca5becb8bef1f149e28fe780e07283
DevPacho <frankjose2004@gmail.com>
2022-08-23 11:50:23 -0500

    [CHANGED] without provider

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,10 +6,8 @@ file { '/etc/ssh/ssh_config':
 
 exec { 'Identity'
     command => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-    provider => 'shell',
 }
 
 exec { 'No password login'
     command => '/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config'
-    provider => 'shell',
 }
