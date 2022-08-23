efb35634b7c6a6d1e1036a2a591b451030fd90b6
DevPacho <frankjose2004@gmail.com>
2022-08-23 02:02:11 -0500

    [CHANGED] trying to concatenate the commands

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 # Client configuration file with Puppet.
 
 exec { 'Configuration to refuse to authenticate using a password':
-    command  => 'eval $(ssh-agent)',
-    command  => 'ssh-add',
+    command  => 'eval $(ssh-agent) && ssh-add',
     provider => 'shell',
 }
