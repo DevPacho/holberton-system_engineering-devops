3cb89a99f9deb97ca73513df91e9ccfda2c430fa
DevPacho <frankjose2004@gmail.com>
2022-08-23 12:04:44 -0500

    [CHANGED] echo path, its provider, relationships/ordering and append to the file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,8 @@
 
 file { '/etc/ssh/ssh_config':
   ensure => present,
-}
--> exec { 'Adding -i flag':
+} -> exec { 'Adding -i flag':
   command  => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-}
--> exec { 'Adding no password in login':
+} -> exec { 'Adding no password in login':
   command  => '/usr/bin/echo "PasswordAuthentication no" >> /etc/ssh/ssh_config'
 }
