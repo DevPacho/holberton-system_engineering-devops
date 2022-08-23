08b1f9d6ae2b701c712fbe5978aea8f5804fda0a
DevPacho <frankjose2004@gmail.com>
2022-08-23 02:29:01 -0500

    [CHANGED] Using file_line Resource (from Puppet's stdlib Module)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Client configuration file with Puppet.
 
 file_line {'disable password login':
-    path => '/etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no',
+    path => '/etc/ssh/sshd_config',
+    line => ' PasswordAuthentication no',
     ensure => 'present',
 }
